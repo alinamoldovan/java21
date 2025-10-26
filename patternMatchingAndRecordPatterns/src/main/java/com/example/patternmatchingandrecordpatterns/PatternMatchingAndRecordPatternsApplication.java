140d7e55ce49b0786a0e652f04750d313fbac21f
Alina <alina.moldovan12@gmail.com>
2025-10-26 16:54:23 +0200

    added module for pattern matching and record pattern

--- patternMatchingAndRecordPatterns/src/main/java/com/example/patternmatchingandrecordpatterns/PatternMatchingAndRecordPatternsApplication.java
+++ patternMatchingAndRecordPatterns/src/main/java/com/example/patternmatchingandrecordpatterns/PatternMatchingAndRecordPatternsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PatternMatchingAndRecordPatternsApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(PatternMatchingAndRecordPatternsApplication.class, args);
     }
 
